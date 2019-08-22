b1f8b00a9fb770a9ba67279c6cb50aab74260331
sbaruah <spandana.baruah@gmail.com>
2019-08-22 04:33:29 -0700

    first commit w most features roughly implemented: no db, no react components for week view

--- app.js
+++ app.js
@@ -6,40 +6,114 @@ const ejs = require("ejs");
 const _ = require("lodash");
 
 const homeStartingContent = "sjbasdfbasdbfhasbdlfkabsdljhbsadhblaasdasdkfcbsakjdbclkasbckasbdclkabskcljabslkcbaskjcbkasbckasjbcks";
-const contactContent = "sdasdknckajsncjanskcnaskjcnka;sdnc;kasn;kansfkvjankjvnkjnv;kdsnvj;dfnsjvd;knvsdkv;ksdfnvdsnv;kdnsvdsnf;vksdkvdkvdsv";
-const aboutContent = "sdasdknckajsncjanskcnaskjcnka;sdnc;kasn;kansfkvjankjvnkjnv;kdsnvj;dfnsjvd;knvsdkv;ksdfnvdsnv;kdnsvdsnf;vksdkvdkvdsv";
+const gpaContent = "Your GPA is: 4.00/4.00";
+const weekContent = "sdasdknckajsncjanskcnaskjcnka;sdnc;kasn;kansfkvjankjvnkjnv;kdsnvj;dfnsjvd;knvsdkv;ksdfnvdsnv;kdnsvdsnf;vksdkvdkvdsv";
+const todoContent = "Here is my to-do list";
 const app = express();
+
 
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(express.static("public"));
 
+
+
+
+// var rows = 24;
+// var columns = 7;
+//
+// var [r, c] = [rows, columns];
+// var filledArray = Array(r).fill().map(() => Array(c).fill(0));
+//
+//
+// function createTable(tableData) {
+//   var table = document.createElement('table');
+//   var tableBody = document.createElement('tbody');
+//
+//   tableData.forEach(function(rowData) {
+//     var row = document.createElement('tr');
+//
+//     rowData.forEach(function(cellData) {
+//       var cell = document.createElement('td');
+//       cell.appendChild(document.createTextNode(cellData));
+//       row.appendChild(cell);
+//     });
+//
+//     tableBody.appendChild(row);
+//   });
+//
+//   table.appendChild(tableBody);
+//   document.body.appendChild(table);
+// }
+//
+// var weekTable = createTable(filledArray);
+
+
+
+
+
+
+
+
+
+let events = [];
 let posts = [];
 
-app.get("/", function(req, res){
+app.get("/", function(req, res) {
   res.render("home", {
     startingContent: homeStartingContent,
+    events: events
+  });
+});
+
+app.get("/weekView", function(req, res) {
+  res.render("weekView", {
+    events: events
+  });
+});
+
+app.get("/todoList", function(req, res) {
+  res.render("todoList", {
+    todoContent: todoContent
+  });
+});
+
+app.get("/gpaCalculator", function(req, res) {
+  res.render("gpaCalculator", {
+    gpaContent: gpaContent
+  });
+});
+
+app.get("/compose", function(req, res) {
+  res.render("compose", {
     posts: posts
-    });
+  });
+  //
+  // posts.push(post);
+  // //
+  // res.redirect("/");
+
 });
 
-app.get("/about", function(req, res){
-  res.render("about", {aboutContent: aboutContent});
+app.post("/compose", function(req, res) {
+  const event = {
+    title: req.body.eventTitle,
+    content: req.body.eventBody
+  };
+
+  events.push(event);
+
+  res.redirect("/");
+
 });
 
-app.get("/contact", function(req, res){
-  res.render("contact", {contactContent: contactContent});
-});
-
-app.get("/compose", function(req, res){
-  res.render("compose");
-});
-
-app.post("/compose", function(req, res){
+app.post("/compose", function(req, res) {
   const post = {
-    title: req.body.postTitle,
-    content: req.body.postBody
+    pTitle: req.body.postTitle,
+    pContent: req.body.postBody
   };
 
   posts.push(post);
@@ -48,10 +122,11 @@ app.post("/compose", function(req, res){
 
 });
 
-app.get("/posts/:postName", function(req, res){
+
+app.get("/posts/:postName", function(req, res) {
   const requestedTitle = _.lowerCase(req.params.postName);
 
-  posts.forEach(function(post){
+  posts.forEach(function(post) {
     const storedTitle = _.lowerCase(post.title);
 
     if (storedTitle === requestedTitle) {
